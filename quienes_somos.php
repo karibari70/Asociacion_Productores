9531bc4c5d7ca1791f1a0959f26f0acc4e971819
Franciscopya <franciscopya98@gmail.com>
2024-08-14 15:02:12 -0300

    Nombre Img Corregidos
    Corregí los nombres de las imágenes para que coincidan con el directorio

--- quienes_somos.php
+++ quienes_somos.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Quienes Somos</title>
     <link rel="stylesheet" href="estilos.css">
 </head>
 
@@ -19,13 +19,13 @@
             <p> La Asociación de Productores de El Hoyo se formó en el año 2012, buscando resolver problemas comunes como la falta de equipamiento para laboreos de suelos, siembra y cosecha. </p>
         </div>
         <div class="img1">
-            <img src="quienes_somos3.jpg" alt="productores1" width=90% />
+            <img src="media/quienes_somos3.jpg" alt="productores1" width=90% />
         </div>
         <div class="text1">
             <p> Actualmente la integramos más de 100 socios, quienes utilizamos los servicios que brinda la Asociación, poniendo a disposición del productor maquinaria para diversas tareas en relación a la producción agrícola, particularmente forrajes.</p>
         </div>
         <div class="img2">
-            <img src="Imagen2.jpg" alt="forrajes" width=70% />
+            <img src="media/Imagen2.jpg" alt="forrajes" width=70% />
         </div>
 
     </div>
@@ -40,14 +40,14 @@
             <div class="historia">
                 <p>El Hoyo se encuentra en un hermoso Valle enclavado en el NO de la Pcia. de Chubut, donde históricamente la cultura y formas de subsistencia de nuestros habitantes se basaban en el trabajo de la tierra y la cría de animales.
                 <p>
-                    <img src="paisaje.JPG" alt="paisaje" width=60%>
+                    <img src="media/paisaje.JPG" alt="paisaje" width=60%>
 
             </div>
 
             <div class="actualidad">
                 <p>Hoy somos emprendedores con ansias de progreso, con el entusiasmo movilizado desde la necesidad de ampliar nuestra producción, cuidando los recursos naturales que usamos a nuestro beneficio.
                 <p>
-                    <img src="Imagen9.jpg" alt="productores3" width=60%>
+                    <img src="media/Imagen9.jpg" alt="productores3" width=60%>
 
             </div>
         </div>
@@ -58,11 +58,11 @@
             <div class="objetivo1">
                 <p> Activar mecanismos que nos permitan incorporar más maquinaria de menor porte para brindar un mejor servicio a los agricultores familiares dedicados a la horticultura y fruticultura.
                 <p>
-                    <img src="imagen_fram.JPG" class ="img3" alt="frambuesa" width=80%>
+                    <img src="media/imagen_fram.JPG" class ="img3" alt="frambuesa" width=80%>
             </div>
             <div class="objetivo2">
                 <p>En cuanto a instalaciones, creemos necesario contar con instalaciones como por ejemplo: molienda de granos, esterilización y encurtidos, alambique, ahumado y envasado al vacio.</p>
-                <img src="imagen_fru.JPG"class="img4" alt="frutilla" width=75%>
+                <img src="media/imagen_fru.JPG"class="img4" alt="frutilla" width=75%>
             </div>
         </div>
     </div>
