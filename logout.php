94f942433fe9f60fb1d2d1e10f0bb8589c589a51
Franciscopya <franciscopya98@gmail.com>
2024-11-04 14:09:17 -0300

    Correcciones

--- logout.php
+++ logout.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 
 session_start();
 session_destroy();
@@ -7,15 +6,3 @@ session_destroy();
 echo "<h1>Te estas desconectado</h1>";
 
 header("refresh:5; url=foro.php");
-=======
-session_start();
-session_destroy();
-
-echo "<p>Finalizado</p>";
-
-header("refresh:3;url=index_loguin.php");
-
-exit();
-
-?>
->>>>>>> 51cf2cccf218ab8dea900378b07a7770d10d5602
